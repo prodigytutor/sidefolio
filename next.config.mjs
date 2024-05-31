b06b136ab179f02d4fc62de4e31b1c9bd41582a2
prodigytutor <david@prodigytutors.pro>
2024-05-31 16:38:07 +0000

    set output

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import remarkGfm from "remark-gfm";
 import rehypePrism from "@mapbox/rehype-prism";
 
 const nextConfig = {
+  output: "export",
   images: {
     domains: ["images.unsplash.com", "res.cloudinary.com"],
   },
